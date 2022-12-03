7b5835bac04336fd063fd18aea62ad02d31a38a7
Sam Richter <hoodie888@outlook.com>
2022-12-03 17:33:14 -0600

    key file

--- stage2/00-configure-apt/00-run.sh
+++ stage2/00-configure-apt/00-run.sh
@@ -3,9 +3,10 @@
 echo "adding repo for edgetpu"
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" > "${ROOTFS_DIR}/etc/apt/sources.list.d/coral-edgetpu.list"
 
-wget -O- https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /etc/apt/trusted.gpg.d/coral-edgetpu.gpg
-#cat files/edgetpu.gpg | gpg --dearmor > "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/coral-edgetpu.gpg"
-#on_chroot apt-key add - < files/edgetpu.gpg
+#wget -O- https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /etc/apt/trusted.gpg.d/coral-edgetpu.gpg
+#cat files/edgetpu.gpg > "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/coral-edgetpu.gpg"
+sudo apt-key adv
+on_chroot apt-key add - < files/edgetpu.gpg
 on_chroot << EOF
 apt-get update
 apt-get dist-upgrade -y
